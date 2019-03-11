a6debfbca82ea3c89b4f4071548bbcc5e8d0b31e
Anthony Totton <novocast7@gmail.com>
2019-03-11 17:25:51 +0000

    basic integration complete

--- integrations/php-app.php
+++ integrations/php-app.php
@@ -16,8 +16,39 @@
  */
 require_once('../vendor/autoload.php');
 
-const APPKEY = 'dSErfa324fSDEs';
-const APPKEY = 'dSErfa324fSDEs';
 
 use \Firebase\JWT\JWT;
 
+class Auth {
+
+    private $cookie = 'stauth';
+    private $key = 'dSErfa324fSDEs';
+    private $hash = ['HS256'];
+
+    public function cookieExists() 
+    {
+        return isset($_COOKIE[$this->cookie]);
+    }
+
+    public function getTokenEmail()
+    {
+        $token = $this->parseCookie();
+
+        return $token['uid'];
+    }
+
+    public function parseCookie()
+    {
+        return JWT::decode( $this->key, $this->hash );
+    }
+}
+
+$auth = new Auth();
+// check for token
+if ($auth->cookieExists()) {
+    // decode token
+    define( 'AUTHED_EMAIL', $this->getUserEmail());
+
+} else {
+    print('ERROR - NO COOKIE');
+}
