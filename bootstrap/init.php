27d791a1083578b94233929ad0890c9b150873a2
Anthony Totton <novocast7@gmail.com>
2019-11-14 11:25:53 +0000

    env fix

--- bootstrap/init.php
+++ bootstrap/init.php
@@ -1,14 +1,22 @@
 <?php
+
 /**
  * Bootstrap the application
  */
+
 use Symfony\Component\Dotenv\Dotenv;
 
 /**
  *  Loads in the env file for this environment
  */
-$dotenv = new Dotenv();
-$dotenv->load(dirname('../') . DIRECTORY_SEPARATOR . '.env');
+try {
+    $dotenv = new Dotenv();
+    $dotenv->load(dirname('../') . DIRECTORY_SEPARATOR . '.env');
+} catch (Exception $e) {
+    // must be on heroku or an environment without an env file.
+    // Heroku automatically has secrets.
+
+}
 
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
